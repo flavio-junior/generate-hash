98468e50ded8aa22c2c8d80c0c25c794d61e8017
Flávio Júnior <59364674+flavio-junior@users.noreply.github.com>
2024-12-03 09:45:14 -0300

    fix: add format of BCryptPasswordEncoder

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,12 @@
 package br.com
 
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder
 
 fun main() {
-    val encoders: MutableMap<String, PasswordEncoder> = HashMap()
-    val pbkdf2Encoder =
-        Pbkdf2PasswordEncoder("", 8, 185000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256)
-    encoders["pbkdf2"] = pbkdf2Encoder
-    val passwordEncoder = DelegatingPasswordEncoder("pbkdf2", encoders)
-    passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder)
-
+    val passwordEncoder: PasswordEncoder = BCryptPasswordEncoder()
     val origin = "admin123"
-    val result = passwordEncoder.encode(origin)
-    println(origin)
-    println("My hash $result")
+    val newValue = passwordEncoder.encode(origin)
+    println("Origin: $origin")
+    println("New Value: $newValue")
 }
